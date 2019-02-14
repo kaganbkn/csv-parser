b3366b8db1e175f5e7acbcf8e3e162221068fbe4
Kağan Beken <kagan.beken@sestek.com>
2019-02-14 16:22:52 +0300

    deneme4

--- pi/Program.cs
+++ pi/Program.cs
@@ -34,6 +34,7 @@ namespace pi
         //deneme
         //deneme2
         //deneme3
+        //deneme4
         static void Main(string[] args)
         {
             args = new[] { "../../../exhibitA-input.csv" };
